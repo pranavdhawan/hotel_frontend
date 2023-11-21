1a49d042823acf9dc1c2b8295b7ea1c6b3e2fe21
Pranav Dhawan <dhawan.pranav8@gmail.com>
2023-11-21 20:45:00 +0530

    sa,fjbkaejGABEKGBLAEW

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,7 +4,7 @@ import useFetch from "../../hooks/useFetch";
 const Featured = () => {
 
   const { data, loading, error } = useFetch(
-    "/hotels/countByCity?cities=berlin,madrid,london"
+    "hotels/countByCity?cities=berlin,madrid,london"
   );
 
 
--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const { data, loading, error } = useFetch("/hotels?featured=true");
+  const { data, loading, error } = useFetch("hotels?featured=true");
 
   return (
     <div className="fp">
