f0b85fa468a738b8fd9d0320052ad0084e5b8e05
Pranav Dhawan <dhawan.pranav8@gmail.com>
2023-11-21 18:37:11 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get(url);
+        const res = await axios.get('/api/'+url);
         setData(res.data);
       } catch (err) {
         setError(err);
