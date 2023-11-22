b8a1cc1dcf544e685fff86efe5a5ba670a4df86f
Pranav Dhawan <dhawan.pranav8@gmail.com>
2023-11-22 14:52:16 +0530

    hehe

---
+++ src/lip/fetch-config.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  // baseURL: 'https://hotel-booking-api-rho.vercel.app/api', 
+  baseURL:"https://hotel-backend-q4cx.onrender.com"
+
+});
